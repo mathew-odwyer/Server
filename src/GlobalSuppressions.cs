ee786dbb4b94c826e1e4582afa5897a9fee73be6
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2025-09-19 02:15:54 +1000

    Initial commit

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+// <copyright file="GlobalSuppressions.cs" company="Software Antics">
+//   Copyright (c) Software Antics. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates", Justification = "KISS")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "See here: https://www.youtube.com/watch?v=FExr6WMn6T8")]
