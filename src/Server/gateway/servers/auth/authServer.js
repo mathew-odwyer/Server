b8f7b2668bedfcaccc2ff91148b4beaa388871ce
Mathew O'Dwyer <mathew.odwyer@protonmail.com>
2025-10-16 21:15:32 +1100

    rework

--- src/Server/gateway/servers/auth/authServer.js
+++
@@ -1,7 +0,0 @@
-/**
- * @description Registers authentication-related RPC methods on the given server.
- * @param {import('../server').Server} server - The RPC server instance on which to register authentication methods.
- */
-export function registerAuthServer(server)
-{
-}
