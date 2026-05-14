3867060f9b2c7a27f3c6935f957be3e520a347f0
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-13 23:12:07 -0700

    Added Gateway mappings

---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Winterhaven.Gateway.Presentation.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+using Winterhaven.Gateway.Presentation.Mappings.Users;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddGatewayMappings(this IServiceCollection services)
+    {
+        services.AddAutoMapper(x =>
+        {
+            x.AddProfile<UserMapper>();
+        });
+
+        return services;
+    }
+}
