ac884657b293cfe37ac5273036319e333c6a4a5f
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-14 02:48:20 -0700

    Fix formatting

--- src/Gateway/Core/Winterhaven.Gateway.Core.Application/Requests/Users/UserRegister/UserRegisterRequest.cs
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Application/Requests/Users/UserRegister/UserRegisterRequest.cs
@@ -14,7 +14,7 @@ using MediatR;
 /// <param name="Password">
 /// Specifies a <see cref="string"/> that represents the password of the user to be registered.
 /// </param>
-public sealed record UserRegisterRequest
-    (string EmailAddress,
+public sealed record UserRegisterRequest(
+    string EmailAddress,
     string Username,
     string Password) : IRequest<UserRegisterResponse>;
