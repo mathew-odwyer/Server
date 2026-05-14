3789135dcb1159e3643c22c1f81ec6caf9d5670f
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-14 02:48:58 -0700

    Create SessionContext.cs

---
+++ src/Gateway/Infrastructure/Winterhaven.Gateway.Infrastructure/Services/Sessions/SessionContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Winterhaven.Gateway.Infrastructure.Services.Sessions;
+
+using Winterhaven.Gateway.Core.Application.Services.Sessions;
+
+internal sealed class SessionContext : ISessionContext, ISessionAuthenticator
+{
+    public string? AccessToken { get; private set; }
+
+    public bool IsAuthenticated
+    {
+        get { return !string.IsNullOrEmpty(this.AccessToken); }
+    }
+
+    public void Authenticate(string accessToken)
+    {
+        this.AccessToken = accessToken;
+    }
+}
