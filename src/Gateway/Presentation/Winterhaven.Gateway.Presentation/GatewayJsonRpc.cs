4cef24a335bd4ee85cfe76bebeed9d8f58b7e35e
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-13 23:09:38 -0700

    Updated GatewayJsonRpc to convert required exceptions to JSON RPC Errors

--- src/Gateway/Presentation/Winterhaven.Gateway.Presentation/GatewayJsonRpc.cs
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/GatewayJsonRpc.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Logging;
 using StreamJsonRpc;
 using StreamJsonRpc.Protocol;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Winterhaven.Common.Exceptions;
 
 internal sealed class GatewayJsonRpc : JsonRpc
 {
@@ -19,15 +22,26 @@ internal sealed class GatewayJsonRpc : JsonRpc
     {
         switch (exception)
         {
-            case InvalidOperationException:
-                return new JsonRpcError.ErrorDetail()
+            case ValidationException validationException:
+                return new JsonRpcError.ErrorDetail
                 {
-                    Code = (JsonRpcErrorCode)1,
-                    Message = "Lol you couldn't login.",
+                    Code = (JsonRpcErrorCode)400,
+                    Message = validationException.Message,
+                    Data = validationException.Errors ?? new Dictionary<string, string[]>
+                    {
+                        { "General", ["One or more validation errors occurred."] }
+                    },
                 };
 
+            case AuthorizationException authorizationException:
+                return new JsonRpcError.ErrorDetail
+                {
+                    Code = (JsonRpcErrorCode)403,
+                    Message = authorizationException.Message,
+                };
+
+            // Everything else: log it server-side, send nothing useful to the client.
             default:
-                // Everything else: log it server-side, send nothing useful to the client.
                 this.logger.LogError(exception, "Unhandled exception processing RPC method '{Method}'.", request.Method);
                 return new JsonRpcError.ErrorDetail()
                 {
