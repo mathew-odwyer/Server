c185adb29db1a1cf17a90d54ea72a1c6a8ef9a19
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-13 23:05:24 -0700

    Updated GlobalSuppressions.cs and moved to Common project

--- src/API/Presentation/Winterhaven.API.Presentation/GlobalSuppressions.cs
+++ src/API/Presentation/Winterhaven.API.Presentation/GlobalSuppressions.cs
@@ -5,4 +5,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Controllers must be public for MVC discovery", Scope = "namespaceanddescendants", Target = "~N:Winterhaven.API.Presentation.Controllers")]
-[assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "DTOs must be public for MVC discovery", Scope = "namespaceanddescendants", Target = "~N:Winterhaven.API.Presentation.DTOs")]
