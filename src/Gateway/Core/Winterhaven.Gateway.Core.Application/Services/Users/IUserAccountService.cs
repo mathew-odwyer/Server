d7c991b029c2c9404f5413f37620fa7dbfc44882
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-15 06:07:35 -0700

    Create IUserAccountService.cs

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Application/Services/Users/IUserAccountService.cs
@@ -0,0 +1,20 @@
+﻿namespace Winterhaven.Gateway.Core.Application.Services.Users;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public sealed record UserLoginResult(
+    string RefreshToken);
+
+public sealed record UserRegistrationResult(
+    bool Success);
+
+public interface IUserAccountService : IDisposable
+{
+    Task<UserRegistrationResult> RegisterUserAsync(string username, string password, string emailAddress, CancellationToken cancellationToken);
+
+    Task<UserLoginResult> LoginUserAsync(string username, string password, CancellationToken cancellationToken);
+
+    Task LogoutUserAsync(CancellationToken cancellationToken);
+}
