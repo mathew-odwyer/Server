79abfb72287fa6e5982cfd50fc5dab261ff6937b
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-15 06:08:04 -0700

    Renamed and fixed AccessBearerToken

---
+++ src/Gateway/Infrastructure/Winterhaven.Gateway.Infrastructure/HTTP/Handlers/AccessTokenHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Winterhaven.Gateway.Infrastructure.HTTP.Handlers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Winterhaven.Gateway.Infrastructure.Services.Sessions;
+
+internal sealed class AccessTokenHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public AccessTokenHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var sessionContext = this.httpContextAccessor.HttpContext?.RequestServices.GetRequiredService<ISessionContext>();
+
+        if (sessionContext is not null && !string.IsNullOrEmpty(sessionContext.AccessToken))
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", sessionContext.AccessToken);
+        }
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
