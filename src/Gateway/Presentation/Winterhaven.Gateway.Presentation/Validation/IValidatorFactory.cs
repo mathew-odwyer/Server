cfc277407a2e048c72cf01da753317d2dece8ace
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-15 15:11:22 +1000

    Created Validation Handling in gateway presentaiton layer

---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Validation/IValidatorFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Winterhaven.Gateway.Presentation.Validation;
+
+using FluentValidation;
+
+internal interface IValidatorFactory
+{
+    IValidator<T>? GetValidator<T>();
+}
---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Validation/Users/UserLoginParametersValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace Winterhaven.Gateway.Presentation.Validation.Users;
+
+using FluentValidation;
+using Winterhaven.Gateway.Presentation.Targets.Users;
+
+internal sealed class UserLoginParametersValidator : AbstractValidator<UserLoginParameters>
+{
+    public UserLoginParametersValidator()
+    {
+        this.RuleFor(x => x.Username)
+            .NotEmpty()
+            .WithMessage("Username must not be empty.");
+
+        this.RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password must not be empty.");
+    }
+}
