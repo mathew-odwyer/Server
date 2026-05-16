7a21d9b8818c3eb6fc28289d7bfc8216a2a2d7cd
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-15 19:27:25 -0700

    Created UserSession value object in Domain layer

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Domain/ValueObjects/Users/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace Winterhaven.Gateway.Core.Domain.ValueObjects.Users;
+
+using System;
+
+public sealed record UserSession(
+    string Username,
+    string AccessToken,
+    string RefreshToken,
+    TimeSpan AccessTokenExpiry);
