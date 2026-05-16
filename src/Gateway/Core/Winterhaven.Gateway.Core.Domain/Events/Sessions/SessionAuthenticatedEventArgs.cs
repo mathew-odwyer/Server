b6f1e0901b894d297930fec8f3b4404c0d8a8395
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-15 19:27:30 -0700

    Create SessionAuthenticatedEventArgs.cs

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Domain/Events/Sessions/SessionAuthenticatedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Winterhaven.Gateway.Core.Domain.Events.Sessions;
+
+using System;
+
+public sealed class SessionAuthenticatedEventArgs: EventArgs
+{
+    public string Username { get; init; }
+
+    public TimeSpan AccessTokenExpiry { get; init; }
+}
