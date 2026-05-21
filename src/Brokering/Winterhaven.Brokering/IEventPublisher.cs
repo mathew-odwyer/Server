9de8070677520526199000948536f908320c2df7
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-21 03:45:32 -0700

    Added docs to IEventPublisher

--- src/Brokering/Winterhaven.Brokering/IEventPublisher.cs
+++ src/Brokering/Winterhaven.Brokering/IEventPublisher.cs
@@ -3,8 +3,26 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+/// <summary>
+///   Defines an interface that represents an event publisher.
+/// </summary>
 public interface IEventPublisher
 {
+    /// <summary>
+    ///   Publishes the specified <typeparamref name="TEvent"/> asynchronously.
+    /// </summary>
+    /// <typeparam name="TEvent">
+    ///   The type of the event to be published.
+    /// </typeparam>
+    /// <param name="e">
+    ///   The event to be published.
+    /// </param>
+    /// <param name="cancellationToken">
+    ///   The cancellation token.
+    /// </param>
+    /// <returns>
+    ///   Returns a <see cref="Task"/> that represents the result of the operation.
+    /// </returns>
     Task PublishEventAsync<TEvent>(TEvent e, CancellationToken cancellationToken = default)
         where TEvent : class;
 }
