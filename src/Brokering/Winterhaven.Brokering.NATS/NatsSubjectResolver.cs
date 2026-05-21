b9eb1af1630a7b33aec98dd3edf80d2b4901f570
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-21 03:44:05 -0700

    Created NatsSubjectResolver

---
+++ src/Brokering/Winterhaven.Brokering.NATS/NatsSubjectResolver.cs
@@ -0,0 +1,18 @@
+﻿namespace Winterhaven.Brokering.NATS;
+
+using System.Reflection;
+using Winterhaven.Brokering.Attributes;
+using Winterhaven.Brokering.NATS.Exceptions;
+
+internal static class NatsSubjectResolver
+{
+    public static string ResolveSubject<TEvent>(TEvent e)
+        where TEvent : class
+    {
+        var type = e.GetType();
+        var attribute = type.GetCustomAttribute<EventNameAttribute>()
+            ?? throw new UnmappedEventException(type);
+
+        return attribute.Name;
+    }
+}
