e9cad9d768b4999662f1f16048bee0b5b96f8299
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-21 03:59:46 -0700

    Added UserLoggedInEvent and UserLoggedOutEvent

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Domain/Events/Users/UserLoggedInEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace Winterhaven.Gateway.Core.Domain.Events.Users;
+
+using Winterhaven.Brokering.Attributes;
+
+/// <summary>
+///   Represents an event that is triggered when a user has logged in.
+/// </summary>
+/// <param name="Username">
+///   The username of the user who has logged in.
+/// </param>
+/// <param name="AccessToken">
+///   The access token of the user who has logged in.
+/// </param>
+[EventName("user.logged_in")]
+public sealed record UserLoggedInEvent(
+    string Username,
+    string AccessToken);
---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Domain/Events/Users/UserLoggedOutEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Winterhaven.Gateway.Core.Domain.Events.Users;
+
+using Winterhaven.Brokering.Attributes;
+
+/// <summary>
+///   Represents an event that is triggered when a user has logged out.
+/// </summary>
+/// <param name="Username">
+///   The username of the user who has logged out.
+/// </param>
+[EventName("user.logged_out")]
+public sealed record UserLoggedOutEvent(
+    string Username);
