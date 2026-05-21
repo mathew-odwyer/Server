54fe06d6e18213f015e71bbe625bd2f17ec70659
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-21 03:18:23 -0700

    Created IEventPublisher.

---
+++ src/Brokering/Winterhaven.Brokering/IEventPublisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Winterhaven.Brokering;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+public interface IEventPublisher
+{
+    Task PublishEventAsync<TEvent>(TEvent e, CancellationToken cancellationToken = default)
+        where TEvent : class;
+}
