df9862c16f923ec122efee7da2358c8f8e6b89d4
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 12:26:56 +1000

    Created PlayerJoinedNotification

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Application/Notifications/Players/PlayerJoinedNotification.cs
@@ -0,0 +1,7 @@
+﻿namespace Winterhaven.Gateway.Core.Application.Notifications.Players;
+
+using Winterhaven.Brokering.Attributes;
+using Winterhaven.Gateway.Core.Domain.ValueObjects.Players;
+
+[NotificationName("player.joined")]
+internal sealed record PlayerJoinedNotification(Player Player);
