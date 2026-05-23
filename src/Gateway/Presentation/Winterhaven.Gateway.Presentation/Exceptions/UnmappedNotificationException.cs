1427fce4219a02f3e57ea6b1be5543553425bfb5
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 12:27:07 +1000

    Created UnmappedNotificationException

---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Exceptions/UnmappedNotificationException.cs
@@ -0,0 +1,30 @@
+﻿namespace Winterhaven.Gateway.Presentation.Exceptions;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+internal sealed class UnmappedNotificationException : Exception
+{
+    public UnmappedNotificationException()
+    {
+    }
+
+    public UnmappedNotificationException(Type notificationType)
+        : base($"Notification type '{(notificationType ?? throw new ArgumentNullException(nameof(notificationType))).FullName}' is missing a [NotificationName] attribute.")
+    {
+        this.NotificationType = notificationType;
+    }
+
+    public UnmappedNotificationException(string message)
+        : base(message)
+    {
+    }
+
+    public UnmappedNotificationException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public Type? NotificationType { get; }
+}
