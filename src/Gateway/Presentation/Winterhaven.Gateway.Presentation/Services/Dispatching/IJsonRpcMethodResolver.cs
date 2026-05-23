2e7873360d94d6c51ea2bf78b49e46001724bc76
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 12:27:19 +1000

    Created JsonRpcMethodResolver

---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Services/Dispatching/IJsonRpcMethodResolver.cs
@@ -0,0 +1,7 @@
+﻿namespace Winterhaven.Gateway.Presentation.Services.Dispatching;
+
+internal interface IJsonRpcMethodResolver
+{
+    string ResolveMethodName<TNotification>(TNotification notification)
+        where TNotification : class;
+}
---
+++ src/Gateway/Presentation/Winterhaven.Gateway.Presentation/Services/Dispatching/JsonRpcMethodResolver.cs
@@ -0,0 +1,21 @@
+﻿namespace Winterhaven.Gateway.Presentation.Services.Dispatching;
+
+using System;
+using System.Reflection;
+using Winterhaven.Brokering.Attributes;
+using Winterhaven.Gateway.Presentation.Exceptions;
+
+internal sealed class JsonRpcMethodResolver : IJsonRpcMethodResolver
+{
+    public string ResolveMethodName<TNotification>(TNotification notification)
+        where TNotification : class
+    {
+        ArgumentNullException.ThrowIfNull(notification);
+
+        var type = typeof(TNotification);
+        var attribute = type.GetCustomAttribute<NotificationNameAttribute>()
+            ?? throw new UnmappedNotificationException(type);
+
+        return attribute.Name;
+    }
+}
