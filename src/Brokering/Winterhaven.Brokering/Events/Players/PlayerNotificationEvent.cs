f414fe3a802bb9952e133f3e640a4a018dd5d4f3
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 21:50:11 +1000

    Setting up player.{UserAccountId}.notify

---
+++ src/Brokering/Winterhaven.Brokering/Events/Players/PlayerNotificationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Winterhaven.Brokering.Events.Players;
+
+using Winterhaven.Brokering.Attributes;
+
+[EventName("player.{UserAccountId}.notify")]
+public sealed record PlayerNotificationEvent(
+    string Method,
+    object? Params);
