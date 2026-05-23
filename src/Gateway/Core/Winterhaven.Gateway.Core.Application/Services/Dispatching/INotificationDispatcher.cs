a2e5f7e81f742408e5edbffd17c21d60c8866516
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 11:27:08 +1000

    Created INotificationDIspatcher for app layer

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Application/Services/Dispatching/INotificationDispatcher.cs
@@ -0,0 +1,17 @@
+﻿namespace Winterhaven.Gateway.Core.Application.Services.Dispatching;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface INotificationDispatcher
+{
+    Task BroadcastAsync<TNotification>(TNotification notification)
+        where TNotification : class;
+
+    Task BroadcastAsync<TNotification>(IEnumerable<Guid> connectionIds, TNotification notification)
+        where TNotification : class;
+
+    Task NotifyAsync<TNotification>(Guid connectionId, TNotification notification)
+                where TNotification : class;
+}
