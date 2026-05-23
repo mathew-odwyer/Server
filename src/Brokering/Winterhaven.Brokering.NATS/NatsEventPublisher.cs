f2a8cd9a4acdf39cbaf29fbec46345c3331265be
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 20:28:57 +1000

    Created NatsEventSubscriber

--- src/Brokering/Winterhaven.Brokering.NATS/NatsEventPublisher.cs
+++ src/Brokering/Winterhaven.Brokering.NATS/NatsEventPublisher.cs
@@ -1,6 +1,7 @@
 ﻿namespace Winterhaven.Brokering.NATS;
 
 using global::NATS.Client.Core;
+using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 using Winterhaven.Brokering.NATS.Resolving;
@@ -9,12 +10,15 @@ internal sealed class NatsEventPublisher : IEventPublisher
 {
     private readonly INatsConnection connection;
 
+    private readonly ILogger<NatsEventPublisher> logger;
+
     private readonly INatsSubjectResolver subjectResolver;
 
-    public NatsEventPublisher(INatsConnection connection, INatsSubjectResolver subjectResolver)
+    public NatsEventPublisher(ILogger<NatsEventPublisher> logger, INatsConnection connection, INatsSubjectResolver subjectResolver)
     {
         this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
         this.subjectResolver = subjectResolver ?? throw new ArgumentNullException(nameof(subjectResolver));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     public async Task PublishEventAsync<TEvent>(TEvent e, CancellationToken cancellationToken = default)
@@ -22,8 +26,12 @@ internal sealed class NatsEventPublisher : IEventPublisher
     {
         ArgumentNullException.ThrowIfNull(e);
 
+        string subject = this.subjectResolver.ResolveSubject<TEvent>();
+
+        this.logger.LogTrace("Publishing NATS Event: {Subject}", subject);
+
         await this.connection.PublishAsync(
-            subject: this.subjectResolver.ResolveSubject(e),
+            subject: subject,
             data: e,
             cancellationToken: cancellationToken).ConfigureAwait(false);
     }
