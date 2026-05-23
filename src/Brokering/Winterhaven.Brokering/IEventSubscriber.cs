0d479b37fb98c9b844a0709643df2375fec162ba
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 16:54:19 +1000

    Create IEventSubscriber for subscribing to events.

---
+++ src/Brokering/Winterhaven.Brokering/IEventSubscriber.cs
@@ -0,0 +1,10 @@
+﻿namespace Winterhaven.Brokering;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+public interface IEventSubscriber
+{
+    Task SubscribeAsync<TEvent>(IEventConsumer<TEvent> consumer, CancellationToken cancellationToken = default)
+        where TEvent : class;
+}
