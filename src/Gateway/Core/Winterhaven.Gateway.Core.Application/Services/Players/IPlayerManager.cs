490c0ccfe4da20932394f3d2165e7365dcc6c2a2
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 11:27:18 +1000

    Created IPlayerManager for app layer

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Application/Services/Players/IPlayerManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Winterhaven.Gateway.Core.Application.Services.Players;
+
+using System;
+using System.Collections.Generic;
+using Winterhaven.Gateway.Core.Domain.ValueObjects.Players;
+
+public interface IPlayerManager
+{
+    void Add(Player player);
+
+    IReadOnlyCollection<Player> GetAllPlayers();
+
+    Player? GetByPlayerId(Guid playerId);
+
+    void Remove(Player player);
+}
