40ad604ed3c03e108f6e658b7e74776ada928a74
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 20:26:26 +1000

    Create IEventSubscribor

---
+++ src/Brokering/Winterhaven.Brokering/IEventSubscriber.cs
@@ -0,0 +1,28 @@
+﻿namespace Winterhaven.Brokering;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+///   Defines an interface that represents an event subscriber.
+/// </summary>
+public interface IEventSubscriber
+{
+    /// <summary>
+    ///   Subscribes the specified <paramref name="consumer"/> to the event bus.
+    /// </summary>
+    /// <typeparam name="TEvent">
+    ///   The type of the event.
+    /// </typeparam>
+    /// <param name="consumer">
+    ///   The consumer to be subscribed to the event bus.
+    /// </param>
+    /// <param name="cancellationToken">
+    ///   The cancellation token.
+    /// </param>
+    /// <returns>
+    ///   Returns a task that is completed when the consumer has subscribed to the event.
+    /// </returns>
+    Task SubscribeAsync<TEvent>(IEventConsumer<TEvent> consumer, CancellationToken cancellationToken = default)
+        where TEvent : class;
+}
