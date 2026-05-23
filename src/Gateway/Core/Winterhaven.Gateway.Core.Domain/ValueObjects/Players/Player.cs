f143ce89127699121aed008ea4b253f0e741c9d2
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-23 11:26:57 +1000

    Created Player Value Object for Gateway

---
+++ src/Gateway/Core/Winterhaven.Gateway.Core.Domain/ValueObjects/Players/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Winterhaven.Gateway.Core.Domain.ValueObjects.Players;
+
+using System;
+
+public sealed record Player(
+    Guid connectionId,
+    Guid playerId,
+    string Name,
+    double X,
+    double Y);
