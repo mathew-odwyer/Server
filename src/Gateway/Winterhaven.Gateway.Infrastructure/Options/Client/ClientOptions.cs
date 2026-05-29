0a696ce14a4658726bb0caced67f85906a78a66d
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-05-29 18:28:41 +1000

    Create ClientOptions and updated appsettings-example.json

---
+++ src/Gateway/Winterhaven.Gateway.Infrastructure/Options/Client/ClientOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Winterhaven.Gateway.Infrastructure.Options.Client;
+
+internal sealed class ClientOptions
+{
+    [Required]
+    public required string ApiKey { get; init; }
+
+    [Required]
+    public required string BaseUrl { get; init; }
+}
