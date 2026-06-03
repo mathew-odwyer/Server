fc814a782d40d1f12b22b868b5e52a023172b11c
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-03 17:23:39 +1000

    🧪 Tests: `UnhandledExceptionFilterAttribute` (#199)
    * [WIP] Tests: `UnhandledExceptionFilterAttribute`
    
    * Added unit tests, passing.

---
+++ src/API/Winterhaven.API.Tests/Presentation/Filters/UnhandledExceptionFilterAttributeTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+using Winterhaven.API.Presentation.Filters;
+
+namespace Winterhaven.API.Tests.Presentation.Filters;
+
+[TestFixture]
+internal sealed class UnhandledExceptionFilterAttributeTests
+{
+    private ActionContext actionContext;
+
+    private ActionDescriptor actionDescriptor;
+
+    private ExceptionContext exceptionContext;
+
+    private UnhandledExceptionFilterAttribute filterAttribute;
+
+    private HttpContext httpContext;
+
+    private ILogger<UnhandledExceptionFilterAttribute> logger;
+
+    private RouteData routeData;
+
+    [Test]
+    public void ClassShouldHaveAttributeUsageAttributeAttached()
+    {
+        // Arrange
+        var type = typeof(UnhandledExceptionFilterAttribute);
+        var attribute = type.GetCustomAttribute<AttributeUsageAttribute>();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(attribute, Is.Not.Null);
+            Assert.That(attribute.AllowMultiple, Is.True);
+            Assert.That(attribute.Inherited, Is.True);
+            Assert.That(attribute.ValidOn, Is.EqualTo(AttributeTargets.Class | AttributeTargets.Method));
+        }
+    }
+
+    [Test]
+    public void ConstructorShouldNotThrowExceptionWhenInvoked() =>
+        // Act and assert
+        Assert.DoesNotThrow(() => new UnhandledExceptionFilterAttribute(logger));
+
+    [Test]
+    public void OnExceptionShouldNotReturnObjectResultWhenExceptionHandled()
+    {
+        // Arrange
+        exceptionContext.ExceptionHandled = true;
+
+        // Act
+        filterAttribute.OnException(exceptionContext);
+
+        // Assert
+        Assert.That(exceptionContext.Result, Is.Null);
+    }
+
+    [Test]
+    public void OnExceptionShouldNotReturnObjectResultWhenExceptionIsNotNotFoundException()
+    {
+        // Arrange
+        exceptionContext.Exception = new InvalidOperationException();
+
+        // Act
+        filterAttribute.OnException(exceptionContext);
+
+        // Assert
+        Assert.That(exceptionContext.Result, Is.Not.TypeOf<InvalidOperationException>());
+    }
+
+    [Test]
+    public void OnExceptionShouldReturnProblemDetailsWhenExceptionIsAnyException()
+    {
+        // Arrange
+        const string message = "An unexpected error occurred. Please try again later.";
+        exceptionContext.Exception = new InvalidOperationException();
+
+        // Act
+        filterAttribute.OnException(exceptionContext);
+
+        // Assert
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(exceptionContext.Result, Is.TypeOf<ObjectResult>());
+
+            var objectResult = exceptionContext.Result as ObjectResult;
+            var problemDetails = objectResult.Value as ProblemDetails;
+
+            Assert.That(problemDetails.Type, Is.EqualTo("https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"));
+            Assert.That(problemDetails.Title, Is.EqualTo("Internal Server Error"));
+            Assert.That(problemDetails.Detail, Is.EqualTo(message));
+            Assert.That(problemDetails.Status, Is.EqualTo(StatusCodes.Status500InternalServerError));
+        }
+    }
+
+    [Test]
+    public void OnExceptionShouldSetExceptionHandledToTrueWhenExceptionIsNotFoundException()
+    {
+        // Arrange
+        exceptionContext.Exception = new InvalidOperationException();
+
+        // Act
+        filterAttribute.OnException(exceptionContext);
+
+        // Assert
+        Assert.That(exceptionContext.ExceptionHandled, Is.True);
+    }
+
+    [Test]
+    public void OnExceptionShouldThrowArgumentNullExceptionWhenContextIsNull() =>
+        // Act and assert
+        Assert.Throws<ArgumentNullException>(() => filterAttribute.OnException(null));
+
+    [SetUp]
+    public void SetUp()
+    {
+        httpContext = new DefaultHttpContext();
+        routeData = new RouteData();
+        actionDescriptor = Substitute.For<ActionDescriptor>();
+        actionContext = new ActionContext(httpContext, routeData, actionDescriptor);
+        exceptionContext = new ExceptionContext(actionContext, Array.Empty<IFilterMetadata>());
+        logger = Substitute.For<ILogger<UnhandledExceptionFilterAttribute>>();
+
+        filterAttribute = new UnhandledExceptionFilterAttribute(logger);
+    }
+}
