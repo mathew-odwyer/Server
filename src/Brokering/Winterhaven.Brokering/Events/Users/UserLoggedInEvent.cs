a682e667661efda6a5f247f6ce17e57b0b395899
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-04 19:45:10 +1000

    Create UserLoggedInEvent.cs

---
+++ src/Brokering/Winterhaven.Brokering/Events/Users/UserLoggedInEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Winterhaven.Brokering.Events.Users;
+
+/// <summary>
+/// </summary>
+/// <param name="Username">
+/// </param>
+/// <param name="AccessToken">
+/// </param>
+public sealed record UserLoggedInEvent(
+    string Username,
+    string AccessToken);
