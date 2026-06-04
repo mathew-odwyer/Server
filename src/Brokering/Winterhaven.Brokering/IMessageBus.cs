d830cf3e0dc6794b2e581eb61d60fcd7f031c6b3
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-04 19:45:06 +1000

    Create IMessageBus.cs

---
+++ src/Brokering/Winterhaven.Brokering/IMessageBus.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Winterhaven.Brokering;
+
+/// <summary>
+/// </summary>
+/// <typeparam name="TData">
+/// </typeparam>
+/// <param name="data">
+/// </param>
+public delegate Task MessageConsumer<TData>(TData data)
+    where TData : class;
+
+/// <summary>
+/// </summary>
+public interface IMessageBus
+{
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TData">
+    /// </typeparam>
+    /// <param name="data">
+    /// </param>
+    /// <param name="cancellationToken">
+    /// </param>
+    public Task PublishAsync<TData>(TData data, CancellationToken cancellationToken = default)
+        where TData : class;
+
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TData">
+    /// </typeparam>
+    /// <param name="consumer">
+    /// </param>
+    /// <param name="cancellationToken">
+    /// </param>
+    public Task SubscribeAsync<TData>(MessageConsumer<TData> consumer, CancellationToken cancellationToken = default)
+        where TData : class;
+}
