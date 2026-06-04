bf586c314a66730d72b8dfcf458d6eec261c96e3
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-04 19:45:15 +1000

    Create UserLoggedOutEvent.cs

---
+++ src/Brokering/Winterhaven.Brokering/Events/Users/UserLoggedOutEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Winterhaven.Brokering.Events.Users;
+
+/// <summary>
+/// </summary>
+/// <param name="Username">
+/// </param>
+public sealed record UserLoggedOutEvent(
+    string Username);
