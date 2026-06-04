3169b9e31572ae0743c95fcc1a9228e58ac5d4a2
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-04 19:45:30 +1000

    Create non-implemented NatsMessageBus

---
+++ src/Brokering/Winterhaven.Brokering.NATS/NatsMessageBus.cs
@@ -0,0 +1,10 @@
+﻿namespace Winterhaven.Brokering.NATS;
+
+internal sealed class NatsMessageBus : IMessageBus
+{
+    public Task PublishAsync<TData>(TData data, CancellationToken cancellationToken = default) where TData : class =>
+        throw new NotImplementedException();
+
+    public Task SubscribeAsync<TData>(MessageConsumer<TData> consumer, CancellationToken cancellationToken = default) where TData : class =>
+        throw new NotImplementedException();
+}
