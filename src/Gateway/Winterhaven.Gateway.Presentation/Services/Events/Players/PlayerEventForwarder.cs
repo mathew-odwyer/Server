bfc57decefa060ba2097b1198503b5b4a1f8233e
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-07 20:29:49 +1000

    Create PlayerEventForwarder.cs

---
+++ src/Gateway/Winterhaven.Gateway.Presentation/Services/Events/Players/PlayerEventForwarder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Winterhaven.Brokering;
+using Winterhaven.Brokering.Events.Players;
+using Winterhaven.Gateway.Core.Application.Services.Users;
+
+namespace Winterhaven.Gateway.Presentation.Services.Events.Players;
+
+internal sealed class PlayerEventForwarder : EventForwarderBase
+{
+    private readonly IUserSessionContext userSessionContext;
+
+    public PlayerEventForwarder(
+        IMessageBus messageBus,
+        IUserSessionContext userSessionContext)
+        : base(messageBus) =>
+        this.userSessionContext = userSessionContext ?? throw new ArgumentNullException(nameof(userSessionContext));
+
+    protected override bool CanForward() =>
+        CanForward() &&
+        userSessionContext.IsAuthenticated &&
+        userSessionContext.UserSession != null;
+
+    protected override async Task RegisterSubscriptionsAsync(CancellationToken cancellationToken = default)
+    {
+        if (!CanForward())
+        {
+            throw new InvalidOperationException("Subscriptions must be registered after the user session has been established.");
+        }
+
+        string userAccountId = userSessionContext.UserSession!.UserAccountId.ToString();
+
+        var subscribeOptions = new SubscribeOptions()
+            .WithRouteKey("playerId", userAccountId);
+
+        await SubscribeAsync<PlayerNotifiedEvent>(OnPlayerNotified, subscribeOptions, cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task OnPlayerNotified(PlayerNotifiedEvent data, CancellationToken cancellationToken) =>
+        await ForwardAsync(data.Method, data.Params, cancellationToken).ConfigureAwait(false);
+}
