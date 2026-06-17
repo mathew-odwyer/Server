f3a01d0f5474903a86173aef0289b6ebc6e9f3c3
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-17 18:44:20 +1000

    Add documentation and `UserLoggedOutEvent` tests

---
+++ src/Common/Winterhaven.Common.Tests/Events/Users/UserLoggedOutEventTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using NUnit.Framework;
+using Winterhaven.Common.Events;
+using Winterhaven.Common.Events.Users;
+
+namespace Winterhaven.Common.Tests.Events.Users;
+
+[TestFixture]
+internal sealed class UserLoggedOutEventTests
+{
+    private string accessToken;
+
+    private UserLoggedOutEvent notification;
+
+    private Guid userAccountId;
+
+    [Test]
+    public void AccessTokenShouldReturnSameTakeWhenInvoked()
+    {
+        // Act
+        string actual = this.notification.AccessToken;
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(this.accessToken));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowArgumentExceptionWhenAccessTokenIsEmpty()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentException>(() => new UserLoggedOutEvent(this.userAccountId, string.Empty));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowArgumentExceptionWhenAccessTokenIsWhiteSpace()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentException>(() => new UserLoggedOutEvent(this.userAccountId, "\r\n\t"));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowArgumentExceptionWhenUserAccountIdIsEmpty()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentException>(() => new UserLoggedOutEvent(Guid.Empty, "accessToken"));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowArgumentNullExceptionWhenAccessTokenIsNull()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentNullException>(() => new UserLoggedOutEvent(this.userAccountId, null));
+    }
+
+    [Test]
+    public void GetPublishEventRouteShouldReturnCorrectRouteWhenInvoked()
+    {
+        // Arrange
+        const string expected = "user.logged_out";
+        var options = new PublishOptions();
+
+        // Act
+        string actual = UserLoggedOutEvent.GetPublishEventRoute(options);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetSubscribeEventRouteShouldReturnCorrectRouteWhenInvoked()
+    {
+        // Arrange
+        const string expected = "user.logged_out";
+        var options = new SubscribeOptions();
+
+        // Act
+        string actual = UserLoggedOutEvent.GetSubscribeEventRoute(options);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.userAccountId = Guid.NewGuid();
+        this.accessToken = "accessToken";
+
+        this.notification = new UserLoggedOutEvent(
+            userAccountId: this.userAccountId,
+            accessToken: this.accessToken);
+    }
+
+    [Test]
+    public void UserAccountIdShouldReturnSameUserAccountIdWhenInvoked()
+    {
+        // Act
+        var actual = this.notification.UserAccountId;
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(this.userAccountId));
+    }
+}
--- src/Common/Winterhaven.Common/Events/Users/UserLoggedOutEvent.cs
+++ src/Common/Winterhaven.Common/Events/Users/UserLoggedOutEvent.cs
@@ -3,15 +3,27 @@
 namespace Winterhaven.Common.Events.Users;
 
 /// <summary>
+///   Represents an event that is published when a user has logged out.
 /// </summary>
 public sealed record UserLoggedOutEvent : IEvent
 {
     private const string EventRoute = "user.logged_out";
 
     /// <summary>
+    ///   Initializes a new instance of the <see cref="UserLoggedOutEvent"/> class.
     /// </summary>
-    /// <param name="userAccountId"></param>
-    /// <param name="accessToken"></param>
+    /// <param name="userAccountId">
+    ///   The user account identifier of the user who has logged out.
+    /// </param>
+    /// <param name="accessToken">
+    ///   The access token of the user who has logged out.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///   Thrown if <paramref name="accessToken"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///   Thrown if <paramref name="accessToken"/> is empty or consists of white space or <paramref name="userAccountId"/> is <see cref="Guid.Empty"/>.
+    /// </exception>
     public UserLoggedOutEvent(Guid userAccountId, string accessToken)
     {
         if (userAccountId == Guid.Empty)
@@ -24,11 +36,19 @@ public sealed record UserLoggedOutEvent : IEvent
     }
 
     /// <summary>
+    ///   Gets the access token of the user who has logged out.
     /// </summary>
+    /// <value>
+    ///   The access token of the user who has logged out.
+    /// </value>
     public string AccessToken { get; init; }
 
     /// <summary>
+    ///   Gets the user account identifier of the user who has logged out.
     /// </summary>
+    /// <value>
+    ///   The user account identifier of the user who has logged out.
+    /// </value>
     public Guid UserAccountId { get; init; }
 
     /// <inheritdoc/>
