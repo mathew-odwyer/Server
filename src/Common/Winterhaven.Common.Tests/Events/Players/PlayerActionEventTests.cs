2241c2e66ae4306395af0de54e53ac7154560fa0
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-17 18:44:40 +1000

    Add documentation and `PlayerActionEvent` tests

---
+++ src/Common/Winterhaven.Common.Tests/Events/Players/PlayerActionEventTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NUnit.Framework;
+using Winterhaven.Common.DTOs.Players;
+using Winterhaven.Common.Events;
+using Winterhaven.Common.Events.Players;
+
+namespace Winterhaven.Common.Tests.Events.Players;
+
+[TestFixture]
+internal sealed class PlayerActionEventTests
+{
+    private PlayerActionDto[] actionQueue;
+
+    private PlayerActionEvent notification;
+
+    private Guid playerId;
+
+    [Test]
+    public void ActionQueueShouldReturnSameActionQueueWhenInvoked()
+    {
+        // Act
+        var actual = this.notification.ActionQueue;
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(this.actionQueue));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowArgumentNullExceptionWhenActionQueueIsNull()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentNullException>(() => new PlayerActionEvent(null));
+    }
+
+    [Test]
+    public void GetPublishEventRouteShouldReturnCorrectRouteWhenInvoked()
+    {
+        // Arrange
+        string expected = $"player.{this.playerId}.action";
+        var options = new PublishOptions()
+            .WithRouteKey("playerId", this.playerId.ToString());
+
+        // Act
+        string actual = PlayerActionEvent.GetPublishEventRoute(options);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetPublishEventRouteShouldThrowArgumentNullExceptionWhenOptionsIsNull()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentNullException>(() => PlayerActionEvent.GetPublishEventRoute(null));
+    }
+
+    [Test]
+    public void GetSubscribeEventRouteShouldReturnCorrectRouteWhenInvoked()
+    {
+        // Arrange
+        string expected = $"player.{this.playerId}.action";
+        var options = new SubscribeOptions()
+            .WithRouteKey("playerId", this.playerId.ToString());
+
+        // Act
+        string actual = PlayerActionEvent.GetSubscribeEventRoute(options);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetSubscribeEventRouteShouldThrowArgumentNullExceptionWhenOptionsIsNull()
+    {
+        // Act and assert
+        Assert.Throws<ArgumentNullException>(() => PlayerActionEvent.GetSubscribeEventRoute(null));
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.playerId = Guid.NewGuid();
+        this.actionQueue =
+        [
+            new PlayerActionDto(
+                Type: "move",
+                MoveX: 1,
+                MoveY: 1,
+                Identifier: 0),
+        ];
+
+        this.notification = new PlayerActionEvent(this.actionQueue);
+    }
+}
--- src/Common/Winterhaven.Common/Events/Players/PlayerActionEvent.cs
+++ src/Common/Winterhaven.Common/Events/Players/PlayerActionEvent.cs
@@ -1,25 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using Winterhaven.Common.DTOs.Players;
 
 namespace Winterhaven.Common.Events.Players;
 
 /// <summary>
+///   Represnts an event that is published when a player should perform an action.
 /// </summary>
 public sealed record PlayerActionEvent : IEvent
 {
     private const string RouteKey = "playerId";
 
     /// <summary>
+    ///   Initializes a new instance of the <see cref="PlayerActionEvent"/> class.
     /// </summary>
-    /// <param name="actionQueue"></param>
+    /// <param name="actionQueue">
+    ///   The action queue to be acted upon.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///   Thrown if <paramref name="actionQueue"/> is <c>null</c>.
+    /// </exception>
     public PlayerActionEvent(PlayerActionDto[] actionQueue)
     {
         this.ActionQueue = actionQueue ?? throw new ArgumentNullException(nameof(actionQueue));
     }
 
     /// <summary>
+    ///   Gets the action queue to be acted upon.
     /// </summary>
-    public PlayerActionDto[] ActionQueue { get; }
+    /// <value>
+    ///   The action queue to be acted upon.
+    /// </value>
+    public IEnumerable<PlayerActionDto> ActionQueue { get; }
 
     /// <inheritdoc/>
     public static string GetPublishEventRoute(PublishOptions options)
