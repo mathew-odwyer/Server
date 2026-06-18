8da0d9fe2c03843a00d71937ed51d5ad8734a01f
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-18 22:04:19 +1000

    Create RoomSeedService.cs
    - Seeding one room (the tavern)
    - Removing all existing rooms, seeding the rooms that are required
    - Essentially, all this done is seed all required rooms, as we create new maps, we need to come here and update the seeds.

---
+++ src/API/Winterhaven.API.Infrastructure/Services/Seeds/Rooms/RoomSeedService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Winterhaven.API.Core.Domain.Entities.Rooms;
+
+namespace Winterhaven.API.Infrastructure.Services.Seeds.Rooms;
+
+internal sealed class RoomSeedService
+{
+    public static void Seed(DbContext databaseContext)
+    {
+        ArgumentNullException.ThrowIfNull(databaseContext);
+
+        var rooms = new List<Room>
+        {
+            new() { MapName = "Bellmare Tavern", MapFilePath = "Bellmare/tavern" }
+        };
+
+        //// Remove all existing rooms before seeding the data, just to be safe.
+        databaseContext.Set<Room>().RemoveRange(databaseContext.Set<Room>());
+
+        // Seed the rooms
+        databaseContext.Set<Room>().AddRange(rooms);
+
+        databaseContext.SaveChanges();
+    }
+}
