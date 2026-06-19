d3bb6743f7b75e74e432b34a06e87087dca8b3c2
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-19 20:28:26 +1000

    Update API entry point to be asynchronous.

--- src/API/Winterhaven.API.Presentation/Program.cs
+++ src/API/Winterhaven.API.Presentation/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Winterhaven.API.Core.Application.Work;
+using Winterhaven.API.Core.Application.Work.Rooms;
 using Winterhaven.API.Infrastructure;
 using Winterhaven.API.Infrastructure.Services.Seeds.Rooms;
 
@@ -8,7 +10,7 @@ namespace Winterhaven.API.Presentation;
 
 internal static class Program
 {
-    internal static void Main(string[] args)
+    internal static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var startup = new Startup(builder.Configuration);
@@ -21,13 +23,16 @@ internal static class Program
         using (var scope = application.Services.CreateScope())
         {
             var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
 
-            var strategy = db.Database.CreateExecutionStrategy();
-            strategy.Execute(() => RoomSeedService.Seed(db));
+            await db.Database.EnsureDeletedAsync().ConfigureAwait(false);
+            await db.Database.EnsureCreatedAsync().ConfigureAwait(false);
+
+            var unitOfWorkFactory = scope.ServiceProvider.GetRequiredService<IUnitOfWorkFactory>();
+            var roomRepository = scope.ServiceProvider.GetRequiredService<IRoomRepository>();
+
+            await RoomSeedService.SeedAsync(unitOfWorkFactory, roomRepository).ConfigureAwait(false);
         }
 
-        application.Run();
+        await application.RunAsync().ConfigureAwait(false);
     }
 }
