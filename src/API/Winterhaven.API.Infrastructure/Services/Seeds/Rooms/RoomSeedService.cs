361317740f93291bd0b6f0fe51b8f4c622e1e3e9
Mathew O'Dwyer <mathew.odwyer@hotmail.com>
2026-06-19 20:28:04 +1000

    Update `RoomSeedService` to use abstractions over `DbContext`

--- src/API/Winterhaven.API.Infrastructure/Services/Seeds/Rooms/RoomSeedService.cs
+++ src/API/Winterhaven.API.Infrastructure/Services/Seeds/Rooms/RoomSeedService.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Winterhaven.API.Core.Application.Work;
+using Winterhaven.API.Core.Application.Work.Rooms;
 using Winterhaven.API.Core.Domain.Entities.Rooms;
 
 namespace Winterhaven.API.Infrastructure.Services.Seeds.Rooms;
 
 internal sealed class RoomSeedService
 {
-    public static void Seed(DbContext databaseContext)
+    public static async Task SeedAsync(IUnitOfWorkFactory unitOfWorkFactory, IRoomRepository roomRepository)
     {
-        ArgumentNullException.ThrowIfNull(databaseContext);
+        ArgumentNullException.ThrowIfNull(unitOfWorkFactory);
+        ArgumentNullException.ThrowIfNull(roomRepository);
 
         var rooms = new List<Room>
         {
             new() { MapName = "Bellmare Tavern", MapFilePath = "Bellmare/tavern" }
         };
 
+        var work = unitOfWorkFactory.CreateUnitOfWork();
+
         //// Remove all existing rooms before seeding the data, just to be safe.
-        databaseContext.Set<Room>().RemoveRange(databaseContext.Set<Room>());
+        roomRepository.DeleteAll();
 
-        // Seed the rooms
-        databaseContext.Set<Room>().AddRange(rooms);
-
-        databaseContext.SaveChanges();
+        // See the rooms.
+        await roomRepository.AddRangeAsync(rooms).ConfigureAwait(false);
+        await work.SaveAsync().ConfigureAwait(false);
     }
 }
